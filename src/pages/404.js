9722f10ba638ed25abee5e63fe147359b2bdfad0
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-06-20 19:24:38 -0300

    Added redirect from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import * as React from "react"
+import { navigate } from 'gatsby'
 
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+  React.useEffect(() => {
+    navigate('/')
+  }, [])
+  return(
   <div>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
-)
+)}
 
 export default NotFoundPage
